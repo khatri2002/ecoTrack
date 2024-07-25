6381ce808dc6e865e7f0ecb0bd856015cd2cf5b9
Jay Khatri <jay@Jays-MacBook-Air.local>
2024-07-25 12:46:14 +0530

    Initial Commit

---
+++ user-app-module/app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+const index = () => {
+    return (
+        <>
+            <View className="border m-20">
+                <Text className="">hello from index</Text>
+            </View>
+        </>
+    );
+}
+
+export default index;
