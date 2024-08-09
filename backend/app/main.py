8376b51e2012467ab0eee6fdb072de8b14d7b387
Jay Khatri <jay@Jays-MacBook-Air.local>
2024-08-09 10:36:17 +0530

    feat(CORS middleware added in server)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import JSONResponse
 from .routers import user
 
 app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return JSONResponse(status_code=200, content={"status": True, "message": "Welcome to the API"})
 
 app.include_router(user.router, prefix="/user", tags=["user"])
